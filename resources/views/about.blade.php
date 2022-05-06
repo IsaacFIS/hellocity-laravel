2f17b7c74e9623b4016ced6561636319ce27840d
FATONKPO S. ISAAC <Isaac>
2022-05-06 15:52:54 +0100

    Add about page update link

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Hello City</title>
+        <title>About-us|Hello City</title>
 
         <!-- Fonts -->
 
@@ -20,12 +20,13 @@
         </style>
     </head>
     <body class="antialiased">
-        <h1>Hello from Cotonou!</h1>
 
-        <p>It's currently {{date('h:i A')}}</p>
+
+        <p>Built with &hearts; by BOSS ISAAC!</p>
+        <p> <a href="/">Revenir vers la page d'accueil</a></p>
 
         <footer>
-            <p>&copy; Copyright {{date('Y')}} &middot; <a href="/about-us">About-us</a></p>
+            <p>&copy; Copyright {{date('Y')}} &middot;</p>
         </footer>
 
     </body>
